a6e3a4610d806acb6d88cd90ad7fb126199424ed
Alexis Vasquez <vasquez.a712@gmail.com>
2025-09-02 19:40:28 -0400

    Added data container

--- lib/ui/dashboard/data/data_container.dart
+++ lib/ui/dashboard/data/data_container.dart
@@ -9,6 +9,16 @@ class DashboardData {
   final String affirmation;
   final List<Insight> insights;
   final List<Expense> topExpenses;
+  final VoidCallback onOpenChatbot;
+  final VoidCallback onViewAllExpenses;
 
   const DashboardData({
     required this.userName, 
+    required this.mood,
+    required this.affirmation,
+    required this.insights,
+    required this.topExpenses,
+    required this.onOpenChatbot,
+    required this.onViewAllExpenses,
+  });
+}
