4dc63307a946fa5202fb397beea22b22de1f1385
Alexis Vasquez <vasquez.a712@gmail.com>
2026-01-08 15:33:00 -0500

    chore: update .gitignore

---
+++ lib/models/user.dart
@@ -0,0 +1,8 @@
+/// track_that_money
+/// lib/models/user.dart
+
+class User {
+  final String name;
+
+  User({required this.name});
+}
---
+++ lib/ui/dashboard/widgets/dashboard_widgets.dart
@@ -0,0 +1,6 @@
+/// Track That Money
+/// lib/ui/dashboard/widgets/dashboard_widgets.dart
+
+export 'header_row.dart';
+export 'insights_grid.dart';
+export 'expenses_list.dart';
