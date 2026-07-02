f1a72e1a248878741859feb9953496b5650fdfd7
alexisvassquez <vasquez.a712@gmail.com>
2026-07-02 13:41:44 -0400

    fixed dead import of JournalScreen and added theme import, removed dead go route for JournalScreen

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'dashboard/screens/dashboard_screen.dart';
-import 'journal/journal_screen.dart';
+import 'theme/theme.dart';
 
 /// Track That Money
 /// lib/ui/app.dart
@@ -13,8 +13,7 @@ class TrackThatMoneyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final router = GoRouter(
       routes: [
-        GoRoute(path: '/', builder: (_, __) => const DashboardScreen()),
-        GoRoute(path: '/journal', builder: (_, __) => const JournalScreen()),
+        GoRoute(path: '/', builder: (_, _) => const DashboardScreen()),
       ],
     );
 
